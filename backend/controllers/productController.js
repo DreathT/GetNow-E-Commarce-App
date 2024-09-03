fc8dc902186b3649ef60c1b085d65ff18fa8aceb
DreathT <abdullahcan123gs@hotmail.com>
2024-09-03 19:08:14 +0300

    API Filters and Pagination functions added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,26 @@
 import Product from "../models/product.js";
 import ErrorHandler from "../utilities/errorHandler.js";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import APIFilters from "../utilities/apiFilters.js";
 
 // Get Products   ==>   /api/v1/products  
 export const getProducts = catchAsyncErrors(async (req, res) => {
-    const products = await Product.find();
+
+    const resPerPage = 4
+
+    const apiFilters = new APIFilters(Product, req.query).search().filters()
+
+    let products = await apiFilters.query;
+    let filteredProductsCount = products.length
+
+    apiFilters.pagination(resPerPage);
+    products = await apiFilters.query.clone();
+
+
     res.status(200).json({
         message: "All Products are these...",
+        resPerPage,
+        filteredProductsCount,
         products
     });
 });
